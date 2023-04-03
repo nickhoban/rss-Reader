36e70b87ddc45bb8a85ada9eedb04824d7c1c5bf
nickhoban <110017566+nickhoban@users.noreply.github.com>
2023-04-03 16:42:34 +0800

    added success

--- js/script.js
+++ js/script.js
@@ -56,6 +56,7 @@ xhr.onload = function() {
   if (xhr.status >=200 && xhr.status <300){
     json = JSON.parse(xhr.responseText)
     console.log(json)
+    console.log("Success!")
     addRSStoDOM(json)
   } else {
     console.log("The request failed")
